f15d9ba580cc78cfe61b7a60f443cd6fa442670c
Will Lewis <will@wflewis.com>
2018-10-08 11:51:34 -0400

    Add the code

--- src/simple-password-generator.cr
+++ src/simple-password-generator.cr
@@ -1,3 +1,38 @@
+require "stdimp/string/puts"
+
 module SimplePasswordGenerator
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
+end
+
+class Options
+  property length : Int32 = 64
+  property count : Int32 = 1
+  property range : Range(Int32, Int32) = 33..126
+end
+
+require "option_parser"
+o = Options.new
+OptionParser.parse! do |parser|
+  parser.banner = "usage: spg [password_length] [num_passwords]"
+  parser.on("-h", "--help", "show this help") { puts parser; exit }
+
+  # add more range options in the future
+  parser.on("-a", "--ascii", "only use printable ASCII characters") { o.range = 33..126 }
+
+  parser.unknown_args do |args|
+    o.length = args[0]?.try(&.to_i) || 64
+    o.count = args[1]?.try(&.to_i) || 1
+  end
+end
+
+o.count.times do
+  b = Bytes.new(o.length)
+  Random::Secure.random_bytes(b)
+  String.build do |builder|
+    b.each do |byte|
+      byte &= 0x7F
+      byte = byte % (o.range.end - o.range.begin + 1) + o.range.begin
+      builder << byte.chr
+    end
+  end.puts
 end
