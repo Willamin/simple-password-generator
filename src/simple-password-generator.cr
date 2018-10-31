80f2bb4c2ecf7c4e20a7e7eaa997f8bae892e7e0
Will Lewis <will@wflewis.com>
2018-10-31 12:40:51 -0400

    Improve usage and error messages

--- src/simple-password-generator.cr
+++ src/simple-password-generator.cr
@@ -1,6 +1,12 @@
 require "stdimp/string/puts"
 require "option_parser"
 
+class OptionParser
+  def arg(option : String, description : String)
+    append_flag(option, description)
+  end
+end
+
 module SimplePasswordGenerator
   alias RangeSet = Set(Range(Int32, Int32))
   VERSION       = {{ `shards version #{__DIR__}`.chomp.stringify }}
@@ -50,12 +56,28 @@ module SimplePasswordGenerator
     end
   end
 
+  def self.arg_try(args, arg_num, default) : Int32
+    a = args[arg_num]?
+    a.try do |a|
+      return a.to_i
+    end
+    default
+  rescue e : ArgumentError
+    STDERR.puts("WARNING: #{a} ignored")
+    default
+  end
+
   def self.handle_args
     Options.new.tap do |o|
       OptionParser.parse! do |parser|
         parser.banner = "usage: spg [password_length] [num_passwords]"
+
+        parser.arg("password_length", "the length of the passwords; defaults to 64 if left blank")
+        parser.arg("num_passwords", "how many passwords to generate; defaults to 1 if left blank")
+
         parser.on("-h", "--help", "show this help") { puts parser; exit }
 
+        parser.separator("character set options:")
         # add more range options in the future
         parse_range("-a", "--ascii", "add all printable ASCII characters to the set", ASCII)
         parse_range("-n", "--number", "add numbers to the set", NUMBERS)
@@ -64,9 +86,11 @@ module SimplePasswordGenerator
         parse_range("-l", "--letter", "add all letters to the set (same as -d -u)", UPPER_LETTERS, LOWER_LETTERS)
         parse_range("-s", "--simple", "add all letters and numbers to the set (same as -n -d -u)", NUMBERS, UPPER_LETTERS, LOWER_LETTERS)
 
+        parser.invalid_option { }
+
         parser.unknown_args do |args|
-          o.length = args[0]?.try(&.to_i) || 64
-          o.count = args[1]?.try(&.to_i) || 1
+          o.length = arg_try(args, 0, 64)
+          o.count = arg_try(args, 1, 1)
         end
       end
 
